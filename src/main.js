386453248a1e2bef4aabf51bff6f41040d967d0b
szk2s <suzukisjss@gmail.com>
2019-06-01 00:04:35 +0900

    Change prettier config

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
 // @ts-nocheck
-import mpeInstrument from "mpe";
+import mpeInstrument from 'mpe';
 
-const addHandler = handler => {
+const addHandler = (handler) => {
   // Request MIDI device access from the Web MIDI API
   /* eslint-disable no-undef */
-  window.navigator.requestMIDIAccess().then(access => {
+  window.navigator.requestMIDIAccess().then((access) => {
     // Iterate over the list of inputs returned
-    access.inputs.forEach(midiInput => {
+    access.inputs.forEach((midiInput) => {
       // Send 'midimessage' events to the mpe.js `instrument` instance
-      midiInput.addEventListener("midimessage", handler);
+      midiInput.addEventListener('midimessage', handler);
     });
   });
 };
@@ -16,7 +16,7 @@ const addHandler = handler => {
 const main = () => {
   // Define `instrument` as an instance of `mpeInstrument`
   const instrument = mpeInstrument({});
-  const onMidiMessage = event => {
+  const onMidiMessage = (event) => {
     console.log(event.timeStamp);
     instrument.processMidiMessage(event.data);
     console.log(instrument.activeNotes());
